ec626f6c8f5bd364ba0cba72848411a9b7010368
bek-211203-coder <bek406197@gmail.com>
2024-06-13 06:08:21 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,303 @@
+// masala 1
+// function sum (arg1, arg2){
+//     let res;
+//     res = arg1+arg2;
+//     console.log(res);
+// }
+// sum(5, 9)
+
+// masala 2
+// function sum (arg1, arg2){
+//     let res;
+//     res = arg1*arg2;
+//     console.log(res);
+// }
+// sum(5, 9)
+
+// masala 3
+// function sum (arg1){
+//     let res;
+//     res = arg1**2;
+//     console.log(res);
+// }
+// sum(5)
+
+// masala 4
+// function sum (arg1){
+//     let res;
+//     res = arg1**3;
+//     console.log(res);
+// }
+// sum(5)
+
+// masala 5
+// function sum(arg1, arg2, arg3) {
+//   let res;
+//   res = (arg1 + arg2 + arg3) / 3;
+//   console.log(res);
+// }
+// sum(5, 9, 6);
+
+// // masala 6
+// function sum (arg1){
+//     let res;
+//     if (arg1%2==0){
+//         res = "true";
+//     }
+//     else {
+//         res = "folse";
+//     }
+//     console.log(res);
+// }
+// sum(5)
+
+// masala 7
+// function sum (arg1){
+//     let res;
+//     if (arg1%2==1){
+//         res = "true";
+//     }
+//     else {
+//         res = "folse";
+//     }
+//     console.log(res);
+// }
+// sum(5)
+
+// masala 8
+// function sum (arg1, arg2){
+//     let res;
+//     if (arg1> arg2){
+//         res = arg1,"kotta son";
+//     }
+//     else {
+//         res = arg2,"kotta son";
+//     }
+//     console.log(res);
+// }
+// sum(5, 9)
+
+// masala 9
+// function sum (arg1, arg2){
+//     let res;
+//     if (arg1< arg2){
+//         res = arg1+"kichik son";
+//     }
+//     else {
+//         res = arg2+"kichik son";
+//     }
+//     console.log(res);
+// }
+// sum(5, 9)
+
+// // masala 10
+// function sum(a){
+//     let b  =  0
+//     for (let i = 1; i<=a; i++  ) {
+//         b = b+i
+//     }
+//     return b
+    
+    
+// }
+// console.log(sum(5));
+
+// masala 11
+// function sum(a){
+//     let b  =  0
+//     for (let i = 1; i<=a; i++  ) {
+//         b = b+i
+//     }
+//     return b    
+// }
+// console.log(sum(5));
+
+
+// masala 12
+// function sum (arg1){
+//     let res;
+//     if (arg1>=18){
+//         res ="kotta";
+//     }
+//     else {
+//         res = "kichik";
+//     }
+//     console.log(res);
+// }
+// sum(17)
+
+
+// masala 13
+// function sum (arg1, arg2){
+//     let res;
+//     if (arg1%arg2==0){
+//         res = "true";
+//     }
+//     else {
+//         res = "folse";
+//     }
+//     console.log(res);
+// }
+// sum(5, 9)
+
+// masala 14
+// function sum (arg1){
+//     let res;
+//     if (ar > 0){
+//         res = "true";
+//     }
+//     else {
+//         res = "folse";
+//     }
+//     console.log(res);
+// }
+// sum(5)
+
+// masala 15
+// function sum (arg1){
+//     let res;
+//     if (arg1<0){
+//         res = "true";
+//     }
+//     else {
+//         res = "folse";
+//     }
+//     console.log(res);
+// }
+// sum(-5)
+
+// masala 19
+
+
+// masal 20 
+// function sum (arg1){
+//     let res;
+//     if (arg1>10){
+//         res = "kotta";
+//     }
+//     else {
+//         res = "kichik";
+//     }
+//     console.log(res);
+// }
+// sum(14)
+
+
+// masal 21
+// function sum (arg1 , arg2){
+//     let sum = arg1 + arg2
+//     let res;
+//     if (sum%2==0){
+//         res = "true";
+//     }
+//     else {
+//         res = "folse";
+//     }
+//     console.log(res);
+// }
+// sum(5, 4)
+
+
+// masala 22
+// function sum (arg1){
+//     let res;
+//     if (arg1%5==0){
+//         res = "true";
+//     }
+//     else {
+//         res = "folse";
+//     }
+//     console.log(res);
+// }
+// sum(-5)
+
+
+// masala 23
+// function sum (arg1 , arg2){
+//     let res;
+//     if (arg1> arg2){
+//         res = "true";
+//     }
+//     else {
+//         res = "folse";
+//     }
+//     console.log(res);
+// }
+// sum(-5 , 5)
+
+
+// masala 24
+// function sum (arg1){
+//     let res;
+//     if (arg1%7==0){
+//         res = "true";
+//     }
+//     else {
+//         res = "folse";
+//     }
+//     console.log(res);
+// }
+// sum(-5)
+
+
+
+// masala 25
+// function sum (arg1){
+//     let res;
+//     res = arg1**10
+//     console.log(res);
+// }
+// sum(2)
+
+
+
+
+// For yordamida ishlanadigan masalalar
+
+// masala 1
+// function sum(){
+//     let x  =  0
+//     for (let i = 1; i<=10; i++  ) {
+//         x = x+i
+//     }
+//     return x
+// }
+// console.log(sum(2));
+
+// masaala 2
+// function sum(){
+//     let x =  1
+//     for (let i = 1; i<=5; i++  ) {
+//         x = x*i
+//     }
+//     return x
+// }
+// console.log(sum(2));
+
+// masala 3
+// function sum(a){
+//     let x  =  0
+//     for (let i = 1; i<=a; i++  ) {
+//         x = x+i
+//     }
+//     return x
+// }
+// console.log(sum(100));
+
+// masala 4
+function sum(a) {
+    for (let i = 2; i <= a; i+=2) {
+        console.log(i);
+    }
+}
+sum(10);
+
+// masala 5
+function sum(a) {
+    for (let i = 1; i <= a; i+=2) {
+        console.log(i);
+    }
+}
+sum(10);
+
+
